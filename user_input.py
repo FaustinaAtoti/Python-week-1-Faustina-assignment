bd17a75946d107c0f0a03961ec4ed6482f46624e
FaustinaAtoti <faustinaatoti@gmail.com>
2024-10-31 17:40:02 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name:")
+
+age = input("Enter your age:")
+location = input("Enter your location:")
+
+print("What's up " + name +"! I know you are " + age + " years old and you live in " + location+". I live there too!")
